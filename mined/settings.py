079607540aa6b09bb108b1fffd394d43ccfbf09d
Phillip Park <ppark9553@gmail.com>
2017-07-29 14:36:34 +0900

    updating keystone restapi

--- mined/settings.py
+++ mined/settings.py
@@ -136,5 +136,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-   os.path.join(STATIC_ROOT, 'safer')
+    os.path.join(STATIC_ROOT, 'admin'),
+    os.path.join(STATIC_ROOT, 'safer')
 ]
