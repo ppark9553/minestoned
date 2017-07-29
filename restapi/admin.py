714efd4887bec4e8721e5ec5fabcb3cf8269ff10
Phillip Park <ppark9553@gmail.com>
2017-07-29 13:46:26 +0900

    updating keystone restapi

--- restapi/admin.py
+++ restapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import KeystoneData
+
+from rest_framework.authtoken.admin import TokenAdmin
+
+TokenAdmin.raw_id_fields = ('user',)
 
 # Register your models here.
+admin.site.register(KeystoneData)
